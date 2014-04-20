e58594a3d58822f694a35d4b9dac5aa6b2083a80
David :Pena <mail>
2014-04-20 17:21:01 -0400

    agregue blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'e762aa1ddc8aab5a06bf10fa96345cbbaa9221a244826a28088585364260066ec59d99e0c7a031d685675acbd7a60f6b758597ac139a338d553820663cf843e4'
